284541040682d0d8f183f103070fde7901a2994a
Elena <lnepostbox@yandex.ru>
2022-09-28 23:33:30 +0300

    aaaaaaaaaaaaa

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,16 +1,21 @@
+DROP TABLE IF EXISTS film_genres;
 DROP TABLE IF EXISTS genres;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS ratings;
+DROP TABLE IF EXISTS friendship;
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE IF NOT EXISTS genres (
                                       genre_id INT PRIMARY KEY,
                                       genre_name VARCHAR(100) NOT NULL
 );
 
-DROP TABLE IF EXISTS ratings;
 CREATE TABLE IF NOT EXISTS ratings (
                                        rating_id INT PRIMARY KEY,
                                        rating_name VARCHAR(100) NOT NULL
 );
 
-DROP TABLE IF EXISTS films;
 CREATE TABLE IF NOT EXISTS films (
                                      film_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                                      film_name VARCHAR(100) NOT NULL,
@@ -20,7 +25,6 @@ CREATE TABLE IF NOT EXISTS films (
                                      rating INT REFERENCES ratings(rating_id)
 );
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users (
                                      user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
                                      email VARCHAR(100),
@@ -29,20 +33,17 @@ CREATE TABLE IF NOT EXISTS users (
                                      birthday DATE
 );
 
-DROP TABLE IF EXISTS friendship;
 CREATE TABLE IF NOT EXISTS friendship (
                                           user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
                                           friend_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
                                           CONSTRAINT composite_friendship_key PRIMARY KEY(user_id, friend_id)
 );
 
-DROP TABLE IF EXISTS film_genres;
 CREATE TABLE IF NOT EXISTS film_genres (
                                            film_id BIGINT NOT NULL REFERENCES films(film_id) ON DELETE CASCADE,
                                            genre_id BIGINT NOT NULL REFERENCES genres(genre_id) ON DELETE CASCADE
 );
 
-DROP TABLE IF EXISTS likes;
 CREATE TABLE IF NOT EXISTS likes (
                                      film_id BIGINT NOT NULL REFERENCES films(film_id) ON DELETE CASCADE,
                                      user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
